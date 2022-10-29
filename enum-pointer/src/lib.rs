84fdeba7935fa979508c20cc2cab53643a4f6889
QuarticCat <QuarticCat@protonmail.com>
2022-10-30 04:38:09 +0800

    merge compactable.rs into lib.rs

--- enum-pointer/src/lib.rs
+++ enum-pointer/src/lib.rs
@@ -1,5 +1,38 @@
-mod compactable;
-
-pub use compactable::*;
-
 pub use enum_pointer_derive::EnumPointer;
+
+pub trait Compactable: Sized {
+    type Pointee;
+    const ALIGN: usize = std::mem::align_of::<Self::Pointee>();
+}
+
+impl<T> Compactable for *const T {
+    type Pointee = T;
+}
+
+impl<T> Compactable for *mut T {
+    type Pointee = T;
+}
+
+impl<'a, T> Compactable for &'a T {
+    type Pointee = T;
+}
+
+impl<'a, T> Compactable for &'a mut T {
+    type Pointee = T;
+}
+
+impl<T> Compactable for Box<T> {
+    type Pointee = T;
+}
+
+impl<'a, T> Compactable for Option<&'a T> {
+    type Pointee = T;
+}
+
+impl<'a, T> Compactable for Option<&'a mut T> {
+    type Pointee = T;
+}
+
+impl<T> Compactable for Option<Box<T>> {
+    type Pointee = T;
+}
