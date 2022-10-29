93aee7bc88f56d19578754503564b3674d1a5c93
QuarticCat <QuarticCat@protonmail.com>
2022-10-30 05:38:27 +0800

    add no_std support

--- enum-pointer/src/lib.rs
+++ enum-pointer/src/lib.rs
@@ -1,8 +1,10 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
 pub use enum_pointer_derive::EnumPointer;
 
 /// # Safety
 ///
-/// - `T` is aligned by `align_of::<Pointee>()`
+/// - `T` is aligned by `align_of::<Pointee>()` (low bits are always zeros)
 /// - `size_of::<T>() == size_of::<usize>()`
 pub unsafe trait Compactable: Sized {
     type Pointee;
@@ -25,6 +27,7 @@ unsafe impl<'a, T> Compactable for &'a mut T {
     type Pointee = T;
 }
 
+#[cfg(feature = "std")]
 unsafe impl<T> Compactable for Box<T> {
     type Pointee = T;
 }
@@ -37,6 +40,7 @@ unsafe impl<'a, T> Compactable for Option<&'a mut T> {
     type Pointee = T;
 }
 
+#[cfg(feature = "std")]
 unsafe impl<T> Compactable for Option<Box<T>> {
     type Pointee = T;
 }
