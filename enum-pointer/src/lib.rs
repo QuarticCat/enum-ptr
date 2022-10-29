2e1d9d42d93e5448e55ca61020b61e89b6eafebc
QuarticCat <QuarticCat@protonmail.com>
2022-10-30 05:34:22 +0800

    make Compactable unsafe

--- enum-pointer/src/lib.rs
+++ enum-pointer/src/lib.rs
@@ -1,38 +1,42 @@
 pub use enum_pointer_derive::EnumPointer;
 
-pub trait Compactable: Sized {
+/// # Safety
+///
+/// - `T` is aligned by `align_of::<Pointee>()`
+/// - `size_of::<T>() == size_of::<usize>()`
+pub unsafe trait Compactable: Sized {
     type Pointee;
-    const ALIGN: usize = std::mem::align_of::<Self::Pointee>();
+    const ALIGN: usize = core::mem::align_of::<Self::Pointee>();
 }
 
-impl<T> Compactable for *const T {
+unsafe impl<T> Compactable for *const T {
     type Pointee = T;
 }
 
-impl<T> Compactable for *mut T {
+unsafe impl<T> Compactable for *mut T {
     type Pointee = T;
 }
 
-impl<'a, T> Compactable for &'a T {
+unsafe impl<'a, T> Compactable for &'a T {
     type Pointee = T;
 }
 
-impl<'a, T> Compactable for &'a mut T {
+unsafe impl<'a, T> Compactable for &'a mut T {
     type Pointee = T;
 }
 
-impl<T> Compactable for Box<T> {
+unsafe impl<T> Compactable for Box<T> {
     type Pointee = T;
 }
 
-impl<'a, T> Compactable for Option<&'a T> {
+unsafe impl<'a, T> Compactable for Option<&'a T> {
     type Pointee = T;
 }
 
-impl<'a, T> Compactable for Option<&'a mut T> {
+unsafe impl<'a, T> Compactable for Option<&'a mut T> {
     type Pointee = T;
 }
 
-impl<T> Compactable for Option<Box<T>> {
+unsafe impl<T> Compactable for Option<Box<T>> {
     type Pointee = T;
 }
