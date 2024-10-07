37d6b8f15aa9abd43212d127fdc4d6c1b9d32b6e
Ryo Onodera <ryoqun@gmail.com>
2024-10-07 15:49:26 +0900

    Improve some documentations

--- enum-ptr/src/traits/aligned.rs
+++ enum-ptr/src/traits/aligned.rs
@@ -70,12 +70,32 @@ mod alloc_impl {
         const ALIGNMENT: usize = align_of::<T>();
     }
 
-    /// It makes assumption on the layout of `Rc`. Use it with caution.
+    /// Implementing [`Aligned`] for [`Rc`] relies on some undocumented
+    /// assumptions on its internal representation. However, it's very unlikely
+    /// for these assumptions to be violated in practice.
+    /// [`RcBox` is marked as `repr(C)` and reference counters are typed as `usize`][1].
+    /// Thus, its alignment is defined as [the highest of its members][2] (i.e. among
+    /// max of `usize` and `T`). Lastly, it's unconceivable for the type to be
+    /// lifted with `repr(C)` as commented or to start to use shorter
+    /// integers as counters.
+    ///
+    /// [1]: https://doc.rust-lang.org/1.81.0/src/alloc/rc.rs.html#286-291
+    /// [2]: https://doc.rust-lang.org/reference/type-layout.html#reprc-structs
     unsafe impl<T> Aligned for Rc<T> {
         const ALIGNMENT: usize = max(align_of::<T>(), align_of::<usize>());
     }
 
-    /// It makes assumption on the layout of `Arc`. Use it with caution.
+    /// Implementing [`Aligned`] for [`Arc`] relies on some undocumented
+    /// assumptions on its internal representation. However, it's very unlikely
+    /// for these assumptions to be violated in practice.
+    /// [`ArcInner` is marked as `repr(C)` and reference counters are typed as `usize`][1].
+    /// Thus, its alignment is defined as [the highest of its members][2] (i.e. among
+    /// max of `usize` and `T`). Lastly, it's unconceivable for the type to be
+    /// lifted with `repr(C)` as commented or to start to use shorter
+    /// integers as counters.
+    ///
+    /// [1]: https://doc.rust-lang.org/1.81.0/src/alloc/sync.rs.html#349-359
+    /// [2]: https://doc.rust-lang.org/reference/type-layout.html#reprc-structs
     unsafe impl<T> Aligned for Arc<T> {
         const ALIGNMENT: usize = max(align_of::<T>(), align_of::<usize>());
     }
@@ -84,12 +104,26 @@ mod alloc_impl {
         const ALIGNMENT: usize = align_of::<T>();
     }
 
-    /// It makes assumption on the layout of `Rc`. Use it with caution.
+    /// On top of the safety reasoning of `impl Align for Rc<T>`, implementing [`Aligned`]
+    /// for `Option<Rc<T>>` is safe as well. `Rc` holds [a pointer to its `RcBox` as
+    /// `NonNull<RcBox<T>>`][1]
+    /// And the `Option`-ed type (i.e. `Option<NonNull<RcBox<T>>>`) is explicitly guaranteed
+    /// to be [same as the original type in terms of size and alignment][2].
+    ///
+    /// [1]: https://doc.rust-lang.org/1.81.0/src/alloc/rc.rs.html#315-322
+    /// [2]: https://doc.rust-lang.org/nightly/std/option/index.html#representation
     unsafe impl<T> Aligned for Option<Rc<T>> {
         const ALIGNMENT: usize = max(align_of::<T>(), align_of::<usize>());
     }
 
-    /// It makes assumption on the layout of `Arc`. Use it with caution.
+    /// On top of the safety reasoning of `impl Align for Arc<T>`, implementing [`Aligned`]
+    /// for `Option<Arc<T>>` is safe as well. `Arc` holds [a pointer to its `ArcInner` as
+    /// `NonNull<ArcInner<T>>`][1]
+    /// And the `Option`-ed type (i.e. `Option<NonNull<ArcInner<T>>>`) is explicitly guaranteed
+    /// to be [same as the original type in terms of size and alignment][2].
+    ///
+    /// [1]: https://doc.rust-lang.org/nightly/src/alloc/sync.rs.html#241-248
+    /// [2]: https://doc.rust-lang.org/nightly/std/option/index.html#representation
     unsafe impl<T> Aligned for Option<Arc<T>> {
         const ALIGNMENT: usize = max(align_of::<T>(), align_of::<usize>());
     }
